519cf6ddbd1e9e4d4e191ea4c0b4552206b9c65a
DONA PAUL <donapaul901@gmail.com>
2025-11-19 11:35:17 +0530

    day 1

---
+++ 30 days of coding challenge/solvemefirst.py
@@ -0,0 +1,19 @@
+# Question:
+
+# Complete the function solveMeFirst(a, b) that takes two integers as input and returns their sum. Then, read two integers from input, call the function, and print the result.
+
+# Input Format:
+
+# - Two integers, one per line.
+
+# answer
+
+def solveMeFirst(a,b):
+    return(a + b)
+	# Hint: Type return a+b below
+
+
+num1 = int(input())
+num2 = int(input())
+res = solveMeFirst(num1,num2)
+print(res)
