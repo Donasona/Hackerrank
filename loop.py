47f95fee6677138c200742a99d907f3de35ee25a
DONA PAUL <donapaul901@gmail.com>
2025-11-19 11:03:59 +0530

    loop

---
+++ loop.py
@@ -0,0 +1,19 @@
+# Write a Python program that takes an integer n as input and prints its multiplication table up to 10 in the format:  
+# n x i = result  
+# where i ranges from 1 to 10.
+
+# answer
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+if __name__ == '__main__':
+    n = int(input().strip())
+    for i in range(1, 11):
+        print(f"{n} x {i} = {n * i}")
+
