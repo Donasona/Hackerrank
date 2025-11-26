b3ff97fc7b879ef0e735d735b49911f22e87e73b
DONA PAUL <donapaul901@gmail.com>
2025-11-26 21:58:15 +0530

    cmt

--- 30 days coding/recursion.py
+++ 30 days coding/recursion.py
@@ -18,12 +18,6 @@ import random
 import re
 import sys
 
-#
-# Complete the 'factorial' function below.
-#
-# The function is expected to return an INTEGER.
-# The function accepts INTEGER n as parameter.
-#
 
 def factorial(n):
     # Write your code here
