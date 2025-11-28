47a996f5d896a3c8928bb90e29fc6023e3fbab3b
DONA PAUL <donapaul901@gmail.com>
2025-11-28 21:14:24 +0530

    cmt

--- 30 days coding/list.py
+++ 30 days coding/list.py
@@ -1,11 +1,11 @@
 if __name__ == '__main__':
     N = int(input())
     arr = []
-    # for _ in range(N):
-    #     command = input().split()
-    #     cmd = command[0]
-    #     args = command[1:]
-    #     if cmd == "print":
-    #         print(arr)
-    #     else:
-    #         eval(f"arr.{cmd}({','.join(args)})")
+    for _ in range(N):
+        command = input().split()
+        cmd = command[0]
+        args = command[1:]
+        if cmd == "print":
+            print(arr)
+        else:
+            eval(f"arr.{cmd}({','.join(args)})")
