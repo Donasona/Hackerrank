7bd619b220832690f28de2919618f35d8e316f16
DONA PAUL <donapaul901@gmail.com>
2025-11-28 21:41:07 +0530

    cmt

--- python coding/capitilize.py
+++ python coding/capitilize.py
@@ -27,10 +27,10 @@ import sys
 def solve(s):
      return ' '.join(word.capitalize() for word in s.split(' '))
      
-# if __name__ == '__main__':
+if __name__ == '__main__':
+    
+    s = input()
 
-#     s = input()
+    result = solve(s)
 
-#     result = solve(s)
-
-#     print(result)
+    print(result)
