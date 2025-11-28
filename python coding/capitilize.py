a03090c7c0564bc022ef4196e2e3ee01d5b6684b
DONA PAUL <donapaul901@gmail.com>
2025-11-28 21:31:01 +0530

    capitilize

---
+++ python coding/capitilize.py
@@ -0,0 +1,17 @@
+# You're given a full name as a string. Your task is to capitalize the first letter of each word (first name, last name, etc.).
+
+# ---
+
+# 🧠 What does “capitalize” mean here?
+
+# If the input is:
+
+# chris alan
+
+
+# The output should be:
+
+# Chris Alan
+
+# answer
+
