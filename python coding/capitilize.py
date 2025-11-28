962ebe69597befd77efb9acd653eed8169705a1f
DONA PAUL <donapaul901@gmail.com>
2025-11-28 21:39:12 +0530

    final capitalize

--- python coding/capitilize.py
+++ python coding/capitilize.py
@@ -28,9 +28,9 @@ def solve(s):
      return ' '.join(word.capitalize() for word in s.split(' '))
      
 if __name__ == '__main__':
+    
     s = input()
 
     result = solve(s)
-    
-    print(result)
 
+    print(result)
