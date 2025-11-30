35e5ceffea355e031b368d55ca1106a891dc91da
DONA PAUL <donapaul901@gmail.com>
2025-11-30 21:31:48 +0530

    cmt

--- python coding/capitilize.py
+++ python coding/capitilize.py
@@ -24,13 +24,14 @@ import re
 import sys
 
 # Complete the solve function below.
-def solve(s):
-     return ' '.join(word.capitalize() for word in s.split(' '))
+
+# def solve(s):
+#      return ' '.join(word.capitalize() for word in s.split(' '))
      
-if __name__ == '__main__':
+# if __name__ == '__main__':
     
-    s = input()
+#     s = input()
 
-    result = solve(s)
+#     result = solve(s)
 
-    print(result)
+#     print(result)
