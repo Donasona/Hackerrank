5671ffdc414b872efee2660cad886c14b2e69731
DONA PAUL <donapaul901@gmail.com>
2025-12-01 21:19:39 +0530

    cmt

--- python coding/nested_list.py
+++ python coding/nested_list.py
@@ -8,9 +8,9 @@
 # answer
 
 if __name__ == '__main__':
-    # records = []
-    # for _ in range(int(input())):
-    #     name = input()
+    records = []
+    for _ in range(int(input())):
+        name = input()
         score = float(input())
         records.append([name, score])
     
